b4ec6bcf5bfa571746d45cb2ac9e21335d5decc1
UriHerrera <hello@uriherrera.com>
2023-02-23 21:51:46 -0600

    fix dep ver

--- scripts/build-deb.sh
+++ scripts/build-deb.sh
@@ -71,7 +71,7 @@ checkinstall -D -y \
 	--pakdir=. \
 	--maintainer=probal31@gmail.com \
 	--provides=booth \
-	--requires="libc6,libgcc-s1,libkf5coreaddons5,libkf5i18n5,libqt5core5a,libqt5gui5,libqt5qml5,libqt5quick5,libqt5widgets5,libqt5multimedia5,libqt5multimediagsttools5,libqt5multimediaquick5,libqt5multimediawidgets5,libstdc++6,mauikit-git \(\>= 2.2.2+git+1\),mauikit-filebrowsing-git \(\>= 2.2.2+git+1\)" \
+	--requires="libc6,libgcc-s1,libkf5coreaddons5,libkf5i18n5,libqt5core5a,libqt5gui5,libqt5qml5,libqt5quick5,libqt5widgets5,libqt5multimedia5,libqt5multimediagsttools5,libqt5multimediaquick5,libqt5multimediawidgets5,libstdc++6,mauikit-git \(\>= 2.2.2+git\),mauikit-filebrowsing-git \(\>= 2.2.2+git\)" \
 	--nodoc \
 	--strip=no \
 	--stripso=yes \
